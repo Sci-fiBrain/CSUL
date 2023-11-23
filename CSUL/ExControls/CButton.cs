d2140a5571cf78733f6395a5aa22ce8d1e085b6a
科幻大脑 <scifibrain@yeah.net>
2023-11-23 21:50:56 +0800

    完成页面雏形

--- CSUL/ExControls/CButton.cs
+++ CSUL/ExControls/CButton.cs
@@ -15,6 +15,9 @@ using System.Windows.Shapes;
 
 namespace CSUL.ExControls
 {
+    /// <summary>
+    /// 自定义按钮控件
+    /// </summary>
     public class CButton : Button
     {
         static CButton()
@@ -25,11 +28,33 @@ namespace CSUL.ExControls
         public static readonly DependencyProperty IconProperty =
             DependencyProperty.Register("Icon", typeof(ImageSource), typeof(CButton), new PropertyMetadata(null));
         public ImageSource Icon
-        {
-            get { return (ImageSource)GetValue(IconProperty); }
-            set { SetValue(IconProperty, value); }
+        {   //图标属性
+            get => (ImageSource)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
         }
 
+        public static readonly DependencyProperty ButtonTypeProperty =
+            DependencyProperty.Register("ButtonType", typeof(CButtonType), typeof(CButton), new PropertyMetadata(CButtonType.Icon));
+        public CButtonType ButtonType
+        {   //按钮类型属性
+            get => (CButtonType)GetValue(ButtonTypeProperty);
+            set => SetValue(ButtonTypeProperty, value);
+        }
 
+        public static readonly DependencyProperty PathDataProperty =
+            DependencyProperty.Register("PathData", typeof(Geometry), typeof(CButton), new PropertyMetadata(null));
+        public Geometry PathData
+        {   //Path类型下的绘制内容
+            get => (Geometry)GetValue(PathDataProperty);
+            set => SetValue(PathDataProperty, value);
+        }
+
+        //public static readonly DependencyProperty RotateProperty =
+        //    DependencyProperty.Register("Rotate", typeof(bool), typeof(CButton), new PropertyMetadata(true));
+        //public bool Rotate
+        //{   //是否旋转
+        //    get => (bool)GetValue(RotateProperty); 
+        //    set => SetValue(RotateProperty, value);
+        //}
     }
 }
---
+++ CSUL/ExControls/CButtonType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSUL.ExControls
+{
+    /// <summary>
+    /// <see cref="CButton"/>的类型枚举
+    /// </summary>
+    public enum CButtonType
+    {
+        Icon,
+        Text,
+        IconText,
+        Path
+    }
+}
